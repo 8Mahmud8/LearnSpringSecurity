5eb2c2a4b1a598de33a44e76d86eeab3b9be1013
Abdullah Al Mahmud <abdullahalmahmud28@gmail.com>
2024-09-08 17:36:31 +0600

    Initial commit

---
+++ src/main/java/com/al_mahmud/LearnSpringSecurity/model/MyUserRepository.java
@@ -0,0 +1,10 @@
+package com.al_mahmud.LearnSpringSecurity.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
